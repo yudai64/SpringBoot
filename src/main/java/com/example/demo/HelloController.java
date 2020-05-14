64f2cddf0cb975a93a88cd30ddded755192d34c2
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-05-14 11:16:38 +0900

    ログをファイルにも出力されるように修正

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.demo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
+  private static Log log = LogFactory.getLog(HelloController.class);
   @RequestMapping("/")
   public String hello() {
-      return "hello";
+
+    if(log.isInfoEnabled()){
+    log.info("HelloControllerクラスが呼ばれた");
+    }
+
+    if(log.isWarnEnabled()){
+      log.warn("警告ログ");
+    }
+
+    return "hello";
+
   }
   
   
