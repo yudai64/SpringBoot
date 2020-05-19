1be79b42a03ed80165f65e97bace8b808cabae47
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-05-19 11:27:54 +0900

    コントローラーにコメント書いた

--- src/main/java/com/example/demo/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/inquiry/InquiryController.java
@@ -28,6 +28,14 @@ public class InquiryController {
     inquiryService = theInquiryService;
   }
 
+  //新規問い合わせページに遷移
+  @GetMapping("/form")
+  public String form(InquiryForm inquiryForm, Model model) {
+    model.addAttribute("title", "新規問い合わせページ");
+    return "inquiry/form";
+  }
+
+  //お問い合わせ一覧ページに遷移
   @GetMapping("index")
   public String index(Model model) {
     List <Inquiry> list = inquiryService.findAll();
@@ -36,18 +44,18 @@ public class InquiryController {
     return "inquiry/index";
   }
 
-  @GetMapping("/form")
-  public String form(InquiryForm inquiryForm, Model model) {
-    model.addAttribute("title", "新規問い合わせページ");
-    return "inquiry/form";
+  //詳細ページに遷移
+  @GetMapping("{id}/details")
+  public String details(@PathVariable("id") Integer id, Model model) {
+    Inquiry inquiry = inquiryService.findById(id);
+    model.addAttribute("title", "お問い合わせ詳細ページ");
+    model.addAttribute("inquiryRequest", inquiry);
+    return "inquiry/details";
   }
 
-  @PostMapping("/form")
-  public String formBack(InquiryForm inquiryForm, Model model) {
-    model.addAttribute("title", "新規問い合わせページ");
-    return "inquiry/form";
-  }
-
+  //新規問い合わせページで確認ボタンが押されたとき
+  //入力に問題がなかった場合、確認画面ページに遷移
+  //問題あった場合は新規問い合わせページに遷移
   @PostMapping("/confirm")
   public String confirm(
     @Validated InquiryForm inquiryForm,
@@ -63,6 +71,14 @@ public class InquiryController {
     return "inquiry/confirm";
   }
 
+  //確認画面ページで戻るボタンが押されたとき、新規問い合わせページに遷移
+  @PostMapping("/form")
+  public String formBack(InquiryForm inquiryForm, Model model) {
+    model.addAttribute("title", "新規問い合わせページ");
+    return "inquiry/form";
+  }
+
+  //確認画面で送信がおされたとき、内容をデータベースに保存してお問い合わせ一覧ページに遷移
   @PostMapping("/index")
   public String save(Inquiry inquiry, InquiryForm inquiryForm, Model model) {
     inquiry.setName(inquiryForm.getName());
@@ -73,14 +89,6 @@ public class InquiryController {
     model.addAttribute("title", "お問い合わせ一覧ページ");
     model.addAttribute("inquiryList", list);
     return "inquiry/index";
-  }
-
-  @GetMapping("{id}/details")
-  public String details(@PathVariable("id") Integer id, Model model) {
-    Inquiry inquiry = inquiryService.findById(id);
-    model.addAttribute("title", "お問い合わせ詳細ページ");
-    model.addAttribute("inquiryRequest", inquiry);
-    return "inquiry/details";
   }
 
 }
