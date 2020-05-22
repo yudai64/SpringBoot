e19278d2be2877aace7f880dac516d1e1340f49a
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-05-22 19:53:52 +0900

    不要なコードを削除

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +10,7 @@ public class HelloController {
 
   private static Log log = LogFactory.getLog(HelloController.class);
   @RequestMapping("/")
-  public String hello(Model model) {
+  public String hello() {
 
     if(log.isInfoEnabled()){
     log.info("HelloControllerクラスが呼ばれた");
@@ -20,12 +19,6 @@ public class HelloController {
     if(log.isWarnEnabled()){
       log.warn("警告ログ");
     }
-
-    model.addAttribute("new_message", "新規問い合わせ");
-    model.addAttribute("new_url", "/inquiry/form");
-    model.addAttribute("index_message", "問い合わせ一覧");
-    model.addAttribute("index_url", "/inquiry/index");
-    model.addAttribute("title", "HelloWorld");
 
     return "hello";
 
